054f41aa543519f1663b88c825b310ca89555b96
unknown <caro.echevarria1991@gmail.com>
2021-04-20 15:14:09 -0500

    JS Current Temp and Weather Conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "fc05c82742f2f3ebf4f9a474f1172d7d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mexico City&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
